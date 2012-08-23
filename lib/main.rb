239ebd413d917ba4f080f70837637915b5d7fccc
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2012-08-23 23:18:22 +0530

    added active record

--- lib/main.rb
+++ lib/main.rb
@@ -4,11 +4,31 @@
 require "rubygems"
 require "nokogiri"
 require "open-uri"
+require 'active_record'
+require 'yaml'
+require 'logger'
 
-url = "http://www.coachfederation.org/about-icf/member-directory/"
-doc = Nokogiri::HTML(open(url))
-doc.at_xpath(".//*[@id='3eff9c15_bd57_4843_8d2f_b5e4b0f14561']/div/div/table/tbody/tr").each do |each_product|
-  puts each_product.at_xpath("b").text
+dbconfig = YAML::load(File.open('db_connection.yml'))
+ActiveRecord::Base.establish_connection(dbconfig)
+ActiveRecord::Base.logger = Logger.new(STDERR)
+ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))
+
+class Location < ActiveRecord::Base
+
+  def initialize
+    p 'Tables details'
+    p Location
+    
+    p 'Count no of rows in table'
+    p Location.count
+  end
+
 end
 
-#puts doc.at_xpath(".//*[@id='3eff9c15_bd57_4843_8d2f_b5e4b0f14561']/div/div/table/tbody/tr/td")
+Location.new
+
+#url = "http://www.junglee.com/mn/search/junglee?ie=UTF8&rh=n%3A803540031&tag=googjuhydr-21&hvadid=13852647285&hvpos=1t1&hvexid=&hvnetw=g&hvrand=1594996003696969763&hvpone=&hvptwo=&hvqmt=e&ref=pd_sl_92zll2ojv_e"
+#doc = Nokogiri::HTML(open(url))
+#doc.css(".product").each do |each_product|
+#  puts each_product.at_css("h3").text
+#end
