cef8d82863e951131d0168c612a702a3a02e8074
MaisaEngineering <contact@maisasolutions.com>
2012-08-25 18:29:25 +0530

    database insertion

--- lib/coach_details.rb
+++ lib/coach_details.rb
@@ -15,32 +15,35 @@ ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'a'))
 
 class Detail < ActiveRecord::Base
 
-  def initialize
+  def get_data
     
     for i in 1..12
       domain = "http://www.associationforcoaching.com"
-      p "http://www.associationforcoaching.com/member_search/ind/accredited/All/?page="+i.to_s
+      #p "http://www.associationforcoaching.com/member_search/ind/accredited/All/?page="+i.to_s
       url = "http://www.associationforcoaching.com/member_search/ind/accredited/All/?page="+i.to_s
       doc = Nokogiri::HTML(open(url))
       for xxx in 1..20     
 
-       puts doc.xpath('html/body/div[2]/div/div/div[3]/div[2]/div/div[3]/ul/li['+xxx.to_s+']/a').text
+       @name = doc.xpath('html/body/div[2]/div/div/div[3]/div[2]/div/div[3]/ul/li['+xxx.to_s+']/a').text
        _href = doc.xpath("html/body/div[2]/div/div/div[3]/div[2]/div/div[3]/ul/li["+xxx.to_s+"]//a").map { |link| link['href'] }
 
        profile_page = domain+_href[0].to_s
-       puts profile_page
+       #puts profile_page
 
        doc1 = Nokogiri::HTML(open(profile_page))
-       _company_name = doc1.xpath("//tr[2]/td").text
+       #_company_name = doc1.xpath("//tr[2]/td").text
+       _is_email = doc1.xpath("//tr[3]/th").text
        _email = doc1.xpath("//tr[3]/td").text
-       _phone_number = doc1.xpath("//tr[4]/td").text
-       _website = doc1.xpath("//tr[5]/td").text
+       #_phone_number = doc1.xpath("//tr[4]/td").text
+       #_website = doc1.xpath("//tr[5]/td").text
 
-       puts "Company Name: "+_company_name
-       puts "Email: "+_email
-       puts "Phone Number: "+_phone_number
-       puts "web site: "+_website
-       
+       #puts "Company Name: "+_company_name
+       p Detail
+       puts "Name: "+@name       
+       puts "Email: "+_email if _is_email == "Email:Level:"
+       #puts "Phone Number: "+_phone_number
+       #puts "web site: "+_website
+       Detail.create!({:name => @name, :email => _email})
       end
       
     end
@@ -66,5 +69,6 @@ end
 
 #details = Detail.new
 #details.creat
-Detail.new
+details = Detail.new
+details.get_data
 
