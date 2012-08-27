c5bcdd8cfd756b9ee3c4823e80cffb5011e2a7c0
MaisaEngineering <contact@maisasolutions.com>
2012-08-27 14:49:38 +0530

    added scrap code for website and phone

--- lib/coach_details.rb
+++ lib/coach_details.rb
@@ -27,23 +27,31 @@ class Detail < ActiveRecord::Base
        @name = doc.xpath('html/body/div[2]/div/div/div[3]/div[2]/div/div[3]/ul/li['+xxx.to_s+']/a').text
        _href = doc.xpath("html/body/div[2]/div/div/div[3]/div[2]/div/div[3]/ul/li["+xxx.to_s+"]//a").map { |link| link['href'] }
 
-       profile_page = domain+_href[0].to_s
+       puts profile_page = domain+_href[0].to_s
        #puts profile_page
 
        doc1 = Nokogiri::HTML(open(profile_page))
        #_company_name = doc1.xpath("//tr[2]/td").text
        _is_email = doc1.xpath("//tr[3]/th").text
        _email = doc1.xpath("//tr[3]/td").text
-       #_phone_number = doc1.xpath("//tr[4]/td").text
-       #_website = doc1.xpath("//tr[5]/td").text
+       _is_phone_number = doc1.xpath("//tr[4]/th").text
+       _phone_number = doc1.xpath("//tr[4]/td").text
+       _is_website = doc1.xpath("//tr[5]/th").text
+       _website = doc1.xpath("//tr[5]/td").text
 
        #puts "Company Name: "+_company_name
-       p Detail
+       #p Detail
        puts "Name: "+@name       
-       puts "Email: "+_email if _is_email == "Email:Level:"
+       puts "Email: "+@email = _email if _is_email == "Email:Level:"
+       puts "Phone: "+@phone = _phone_number if _is_phone_number == "Tel:"
+       puts "Website: "+@website = _website if _is_website == "Website:"
        #puts "Phone Number: "+_phone_number
        #puts "web site: "+_website
-       Detail.create!({:name => @name, :email => _email})
+       #Detail.create!({:name => @name, :email => @email, :phone => @phone, :website => @website })
+       @name = nil
+       @email = nil
+       @phone = nil
+       @website = nil
       end
       
     end
